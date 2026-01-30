7ccd6de38a0d210344736b8b2ed07d3ee7b688b6
lve-qw <9067457468@mail.ru>
2026-01-30 22:50:22 +0300

    fix: model ID fuild

--- internal/model/product.go
+++ internal/model/product.go
@@ -1,16 +1,18 @@
 // Пакет model содержит структуры данных, используемые в приложении.
 package model
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 // Product представляет товар одежды, хранится в MongoDB.
 type Product struct {
-	ID           string   `bson:"_id,omitempty" json:"id,omitempty"`
-	Name         string   `bson:"name" json:"name" validate:"required"`
-	URL          string   `bson:"url" json:"url" validate:"required,url"`
-	Price        int      `bson:"price" json:"price" validate:"required,min=0"`
-	Brand        string   `bson:"brand" json:"brand" validate:"required"`
-	PhotosURLs   []string `bson:"photos_urls" json:"photos_urls" validate:"required,dive,url"`
-	Availability bool     `bson:"availability" json:"availability"`
-	Description  string   `bson:"description" json:"description"`
+	ID           primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Name         string             `bson:"name" json:"name" validate:"required"`
+	URL          string             `bson:"url" json:"url" validate:"required,url"`
+	Price        int                `bson:"price" json:"price" validate:"required,min=0"`
+	Brand        string             `bson:"brand" json:"brand" validate:"required"`
+	PhotosURLs   []string           `bson:"photos_urls" json:"photos_urls" validate:"required,dive,url"`
+	Availability bool               `bson:"availability" json:"availability"`
+	Description  string             `bson:"description" json:"description"`
 }
-
-
